5ba555d48a239641161cbab0695a2da7b1c757d4
fathimahanaan <hanaanfh764@icloud.com>
2026-05-01 22:42:25 +0100

    hey

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -57,7 +57,7 @@ export default function Home() {
           {/* Right Image */}
           <div className="md:w-1/2 flex justify-center">
             <img
-              src="/images/tres.png"
+              src="/images/image2.png"
               alt="Bakery Items"
               loading="lazy"
               className="w-full max-w-md md:max-w-lg object-contain"
